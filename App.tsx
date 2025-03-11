77b1a6657a585e39ebf2f99468d5e47e7d46efed
MizginYildirak <mizgin_yldrak@hotmail.com>
2025-03-11 23:25:28 +0300

    feat: create bottomtabbar

--- App.tsx
+++ App.tsx
@@ -2,15 +2,32 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import Chat from "./screens/Chat";
+import Chats from "./screens/Chats";
+import Updates from "./screens/Updates";
+import Groups from "./screens/Groups";
+import Calls from "./screens/Calls";
+
+const BottomTabs = createBottomTabNavigator();
+
+function BottomTabNavigator() {
+  return (
+    <BottomTabs.Navigator>
+      <BottomTabs.Screen name="Chats" component={Chats} />
+      <BottomTabs.Screen name="Updates" component={Updates} />
+      <BottomTabs.Screen name="Groups" component={Groups} />
+      <BottomTabs.Screen name="Calls" component={Calls} />
+    </BottomTabs.Navigator>
+  );
+}
 
 const Stack = createStackNavigator();
 
 function ChatStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Chat" component={Chat} />
+      <Stack.Screen name="Home" component={BottomTabNavigator} />
     </Stack.Navigator>
   );
 }
